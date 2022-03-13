d7199bfc809f13a4836e821c0accfdd1bd487127
Crazelu <eberelucky64@gmail.com>
2022-03-13 22:02:02 +0100

    Initial Commit

---
+++ Notepad/NotepadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotepadApp.swift
+//  Notepad
+//
+//  Created by Crazelu on 13/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NotepadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
